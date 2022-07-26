a3dff8e7a4fbd023c2c253a48a19d98bb62ce615
MAC <mac@MACs-MacBook-Pro.local>
2022-07-26 20:54:50 +0100

    added Agents component

---
+++ src/components/about/OtherInfo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <h2>Other Info</h2>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci erat vestibulum ac non. Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci erat vestibulum ac non. Lorem 
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci erat vestibulum ac non. Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci erat vestibulum ac non. Lorem 
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.container{
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    padding: 0 40px;
+    justify-content: center;
+    height: 400px;
+}
+
+p{
+    margin-top:20px;
+}
+
+@media (min-width:640px) {
+    p{
+        width:80%;
+    }
+    
+}
+@media (min-width:1024px){
+    .container{
+        padding: 0 100px;
+    }
+}
+</style>
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -3,6 +3,8 @@
         <AboutHeader />
         <AboutUs />
         <TeamComponent />
+        <Agents />
+        <OtherInfo />
     </div>
 </template>
 
@@ -10,11 +12,40 @@
 import AboutHeader from '../components/about/AboutHeader.vue';
 import AboutUs from '../components/about/AboutUs.vue';
 import TeamComponent from '../components/about/TeamComponent.vue';
+import Agents from '../components/home/Agents.vue';
+import OtherInfo from '../components/about/OtherInfo.vue';
 export default {
-    components: { AboutHeader, AboutUs, TeamComponent}
+    components: { AboutHeader, AboutUs, TeamComponent, Agents, OtherInfo }
 }
 </script>
 
 <style>
+#about{
+    color:#6d553e;
+}
+
+h2 {
+  font-size: 32px;
+}
+
+h3{
+    font-size: 24px;
+}
+
+p{
+    font-size: 0.8rem;
+}
+
+@media (min-width:640px){
+    p{
+        font-size: 1rem;
+    }
+}
+
+@media (min-width:1024px){
+    h2{
+        font-size: 44px;
+    }
+}
 
 </style>
