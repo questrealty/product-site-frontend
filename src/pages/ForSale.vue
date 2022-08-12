4cc546634fce81380fde544653e2032a24bcb996
motuncoded <motunrayoadeneye@example.com>
2022-08-12 14:16:58 +0100

    update

--- src/pages/ForSale.vue
+++ src/pages/ForSale.vue
@@ -1,12 +1,20 @@
 <template>
-  <div>
-    <h1>For Sale page</h1>
-    <p>Coming soon ...</p>
-  </div>
+    <div id="about">
+        <SaleHeader />
+        <AboutUs />
+        <TeamComponent />
+    </div>
 </template>
 
 <script>
-export default {};
+import SaleHeader from '../components/sales/SaleHeader.vue';
+import AboutUs from '../components/about/AboutUs.vue';
+import TeamComponent from '../components/about/TeamComponent.vue';
+export default {
+    components: { AboutHeader, AboutUs, TeamComponent}
+}
 </script>
 
-<style></style>
+<style>
+
+</style>
