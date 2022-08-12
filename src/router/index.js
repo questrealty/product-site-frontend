707f6ca8f3c80dd03daa6582b2ab3b534b49c612
motuncoded <motunrayoadeneye@example.com>
2022-08-12 15:48:35 +0100

    update

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../pages/Home.vue";
 import About from "../pages/About.vue";
 import ForRent from "../pages/ForRent.vue";
-import ForSale from "../pages/ForSale.vue";
 import Login from "../pages/Login.vue";
 import Register from "../pages/Register.vue";
 
@@ -22,11 +21,7 @@ const routes = [
     name: "ForRent",
     component: ForRent,
   },
-  {
-    path: "/for-sale",
-    name: "ForSale",
-    component: ForSale,
-  },
+
   {
     path: "/login",
     name: "Login",
