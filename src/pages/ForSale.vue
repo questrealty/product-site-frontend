c3f0e1e2671647459a8a8cf74f91fe38afb714ef
motuncoded <motunrayoadeneye@example.com>
2022-08-16 07:38:50 +0100

    created login and register pages

---
+++ src/pages/ForSale.vue
@@ -0,0 +1,32 @@
+<template>
+  <Header/>
+  <div class="container">
+  
+    <FilterBar />
+    <SearchHeader />
+    <div class="content">
+      <PropertyList />
+      <SideBar />
+    </div>
+  </div>
+  <SiteFooter/>
+</template>
+
+
+<script setup>
+import FilterBar from "../components/forSale/FilterBar.vue";
+import PropertyList from "../components/forSale/PropertyList.vue";
+import SideBar from "../components/forSale/SideBar.vue";
+import SearchHeader from "../components/forSale/SearchHeader.vue";
+
+import Header from "../components/Header.vue";
+import SiteFooter from "../components/SiteFooter.vue";
+</script>
+
+<style scoped>
+.content {
+  display: flex;
+  justify-content: space-between;
+  margin: 20px;
+}
+</style>
