6bee077242e2867f92abb6096afc0d2a081defee
Gameznut <emmanuelapabiekun@gmail.com>
2022-08-18 21:51:08 +0100

    404 created test 4

--- src/components/icons/NotFoundBackground.vue
+++ src/components/icons/NotFoundBackground.vue
@@ -1,7 +1,7 @@
 <template>
   <svg
-    width=""
-    height=""
+    width="1131"
+    height="517"
     viewBox="0 0 1131 517"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
@@ -491,4 +491,5 @@
       </linearGradient>
     </defs>
   </svg>
+  
 </template>
--- src/pages/PagesNotFound.vue
+++ src/pages/PagesNotFound.vue
@@ -1,33 +1,72 @@
 <template>
-  <div class="grid place-items-center h-screen">
-    <div>
-      <!-- <div class="">
-        <h1 class="h1-med !text-[64px] !leading-[76.8px] mb-[21px]">Oops!</h1>
+  <div
+    class="
+      mx-auto
+      flex flex-col
+      justify-center
+      items-center
+      h-screen
+      landscape:min-h-fit
+      max-w-[1440px]
+      relative
+    "
+  >
+    <div
+      class="
+        self-end
+        absolute
+        md:top-[100px]
+        top-[40px]
+        landscape:top-[20px] landscape:lg:right-[64px] landscape:lg:top-[104px]
+      "
+    >
+      <div class="items-left bg--500 px-[20px] flex flex-col items-end">
+        <h1
+          class="
+            text-[64px]
+            leading-[76.8px]
+            landscape:lg:text-[64px]
+            font-[500]
+            landscape:mb-[10px] landscape:text-[50px] landscape:leading-[30px]
+          "
+        >
+          Oops!
+        </h1>
         <p
-          class="h1-reg !text-[18px] max-w-[420px] !leading-[21.6px] mb-[30px]"
+          class="
+            max-w-[400px]
+            text-right text-[18px]
+            landscape:text-right
+            leading-[21.6px]
+            md:my-[21px]
+            mb-[5px]
+            mt-[10px]
+          "
         >
           This Page doesn’t exist or has been removed! We suggest you back to
           home
         </p>
-        <router-link to="/"
-          ><p
-            class=" w-max
-              p-[10px]
-              bg-[#6D553E]
-              rounded-[8px]
-              text-[48px]
-              leading-[57.8px]
-              font-[400]
-              text-[white]
-            "
-          >
-            Back To Home
-          </p></router-link
+        <div
+          class="
+            p-[10px]
+            bg-[#6D553E]
+            text-white
+            rounded-[8px]
+            sm:text-[40px]
+            text-[30px]
+            font-[400]
+            text-center
+            w-full
+            max-w-max
+            leading-[57px]
+          "
         >
-      </div> -->
-      <div class="w-[900px] p-[10px]">
-        <NotFoundBackground class="" />
+          <router-link to="/"> Back To Home </router-link>
+        </div>
       </div>
+    </div>
+    <div class="h-fit">
+      <NotFoundBackground class="max-w-full landscape:h-fit" />
     </div>
   </div>
 </template>
@@ -35,5 +74,3 @@
 <script setup>
 import NotFoundBackground from "../components/icons/NotFoundBackground.vue";
 </script>
-
-<style></style>
